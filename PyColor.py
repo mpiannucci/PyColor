27f069906cb92e69ec28ee9aa6c30986843b4c98
Matthew Iannucci <mpiannucci@gmail.com>
2014-07-31 21:21:17 -0400

    Added static methods and new readme

--- PyColor.py
+++ PyColor.py
@@ -6,9 +6,28 @@ class PyColor:
     RED = '\033[91m'
     DEFAULT = '\033[0m'
 
-    def Disable(self):
-        self.PURPLE = ''
-        self.BLUE = ''
-        self.GREEN = ''
-        self.YELLOW = ''
-        self.RED = ''
+    @staticmethod
+    def Disable():
+        '''Abstracion to simply turn off color printing'''
+        return PyColor.DEFAULT
+
+    @staticmethod
+    def getColorText(color, text):
+        '''Return a colored text block
+
+        @param color Color def from the static class variables
+        @param text The text to print to print to the console
+
+        '''
+        return color + text + PyColor.DEFAULT
+
+    @staticmethod
+    def printColorText(color, text):
+        '''Print a line of text in the color of your choosing
+
+        @param color Color def from the static class variables
+        @param text The text to print to the console
+
+        '''
+        print color + text + PyColor.DEFAULT
+        return True
--- README.md
+++ README.md
@@ -5,7 +5,22 @@ Module to print to the terminal in color with python!
 
 Usage 
 --------
+```python
+from PyColor import PyColor
 
-        from PyColor import PyColor
+print PyColor.GREEN + 'This is green!' + PyColor.DEFAULT
+```
 
-        print PyColor.GREEN + 'This is green!' + PyColor.DEFAULT
+You may also get a line of colored text to use later:
+```python
+
+text = "I am successful!"
+statusText = PyColor.getColorText(PyColor.GREEN, text)
+print statusText
+```
+
+Or use the function to print it for you:
+```python
+text = "I am successful!"
+statusText = PyColor.printColorText(PyColor.GREEN, text)
+```
